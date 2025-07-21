21862869c5977b2fe96049b48982247e1cbacb3c
wangshunshun1020 <110956499+wangshunshun1020@users.noreply.github.com>
2025-07-21 10:52:56 +0800

    28

--- addons/carswxsys/model/Brand.php
+++ addons/carswxsys/model/Brand.php
@@ -1,84 +1,72 @@
 <?php
-    
-    namespace addons\carswxsys\model;
 
-    
-    class Brand extends BaseModel
+namespace addons\carswxsys\model;
+
+
+class Brand extends BaseModel
+{
+
+
+    protected $name = 'carswxsys_brand';
+
+    public function getListByWhere($map, $od)
     {
-    
-    
-        protected $name = 'carswxsys_brand';
-        
-        public  function getListByWhere($map,$od)
-        {
-            $list =  $this
-                ->field('id,name,thumb')
-                ->where($map)
-                ->order($od)
-                ->select();
-            
+        $list = $this
+            ->field('id,name,thumb')
+            ->where($map)
+            ->order($od)
+            ->select();
 
-            
-            if($list)
-            {
-                foreach ($list as $k=>$v)
-                {
-                    if($v['thumb'])
-                        $list[$k]['thumb'] = cdnurl($v['thumb'], true);
-                    
-                    
-                }
-                
+
+        if ($list) {
+            foreach ($list as $k => $v) {
+                if ($v['thumb'])
+                    $list[$k]['thumb'] = cdnurl($v['thumb'], true);
+
+
             }
-            
-            
-            
-            
-            return  $list;
-            
+
         }
-        
-        
-    
-        
-        
-        
-        public static function getOne($id)
-        {
-            
-            $brandinfo = self::where('id', '=', $id)->find();
-            
-            
-            
-            return $brandinfo;
-            
-        }
-        
-        
-        public  function getBrandlistgroup($map)
-        {
-            
-            
-            $brandlist = $this->where($map)->group('firstname')->order('firstname')
-                ->select();
-            
-         
-            
-            if($brandlist)
-            {
-                foreach ($brandlist as $k=>$v)
-                {
-    
-                    $brandlist[$k]['thumb'] =cdnurl($v['thumb'], true);
-                    
-                    
-                }
-                
-            }
-            
-            
-            
-            return $brandlist;
-        }
-        
+
+
+        return $list;
+
     }
+
+
+    public static function getOne($id)
+    {
+
+        $brandinfo = self::where('id', '=', $id)->find();
+
+
+        return $brandinfo;
+
+    }
+
+
+    public function getBrandlistgroup($map)
+    {
+
+        // 获取所有符合条件的数据，按 firstname 排序
+        $allBrands = $this->where($map)
+            ->order('firstname')
+            ->select()
+            ->toArray(); // ThinkPHP5中记得加 toArray()
+
+        $groupedBrands = [];
+
+        foreach ($allBrands as $brand) {
+            $key = $brand['firstname'];
+            // 每个 firstname 只取第一个品牌作为代表
+            if (!isset($groupedBrands[$key])) {
+                $brand['thumb'] = cdnurl($brand['thumb'], true); // 处理图片路径
+                $groupedBrands[$key] = $brand;
+            }
+        }
+
+        // 转为索引数组返回
+        return array_values($groupedBrands);
+    }
+
+}
